9f6140244ffe169bd6d3a0df5b65826b75ef659c
Adelson Lira <adelsonlira@yahoo.com.br>
2021-11-01 23:54:59 -0300

    Atualizando títulos h5

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,11 +12,11 @@ function App() {
         <h1 className="text-primary py-3">Dashboard de Vendas</h1>
         <div className="row px-3">
           <div className="col-sm-6">
-            <h5 className="text-center text-secundary">Taxa de Sucesso (%)</h5>
+            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
             <BarChart />
           </div>
           <div className="col-sm-6">
-            <h5 className="text-center text-secundary">Todas Vendas</h5>
+            <h5 className="text-center text-secondary">Todas Vendas</h5>
             <DonutChart />
           </div>
         </div>
