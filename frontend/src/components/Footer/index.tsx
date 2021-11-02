014db19843c6dc3cf6f4024c2d3113f6f12eb750
Adelson Lira <adelsonlira@yahoo.com.br>
2021-11-01 23:12:23 -0300

    Configurando FrontEnd - Página Estática

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://github.com/adelsonlira" target="_blank" rel="noreferrer">Adelson Lira</a></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br />
+          Evento promovido pela escola DevSuperior: <a href="https://instagram.com/adelsonlira" target="_blank" rel="noreferrer">@adelsonlira</a></small></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
